5b7dbca1d52c4b927f4e6b34ca8d7b910bf62260
Galads <nikola_2212@mail.ru>
2021-09-11 19:25:42 +0300

    m to mac

---
+++ Shumkin/tasks/src/main/kotlin/com/github/galads/sem02/Histogram.kt
@@ -0,0 +1,18 @@
+package com.github.galads.sem02
+
+import java.io.File
+
+object Histogram {
+    @JvmStatic
+    fun main(arg: Array<String>) {
+        val map = File("src/main/kotlin/com/github/galads/sem02/input")
+            .readLines()
+            .associate {
+                val pair = for (i .. it) {
+                    i1
+                }
+                pair to pair
+            }
+        println(map)
+    }
+}
