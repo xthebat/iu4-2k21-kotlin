b146a0c6f1581686242f14ffa00c8455f2a76698
Galads <nikola_2212@mail.ru>
2021-09-14 05:41:22 +0300

    add sem02/histogram

--- Shumkin/tasks/src/main/kotlin/com/github/galads/sem02/Histogram.kt
+++ Shumkin/tasks/src/main/kotlin/com/github/galads/sem02/Histogram.kt
@@ -1,9 +1,9 @@
 package com.github.galads.sem02
 
 import java.io.File
-import kotlin.random.Random
+import kotlin.math.roundToInt
 
-private fun countCharacters(str: String, chr:Char): Int {
+private fun countCharacters(str: String, chr: Char): Int {
     var cnt = 0
 
     for (i in str)
@@ -20,59 +20,67 @@ private fun arrCountCharacters(arrStr: List<String>, chr: Char): Int {
     return cnt
 }
 
-private fun getEmptyIndex(arr:Array<Double?>):Int? {
-    for ((id, elem) in arr.withIndex()) {
+private fun getEmptyIndexArr(arr: Array<Pair<Char, Double>?>): Int? {
+    for ((id, elem) in arr.withIndex())
         if (elem == null)
             return id
-    }
     return null
 }
 
-private fun probabilityGenerate(map: List<String>, assMap: MutableMap<Char, Int>) {
+private fun writeListToFile(arr: List<Char>, name: String = "${System.getProperty("user.dir")}/file.out") {
+    File("src/main/kotlin/com/github/galads/sem02/file.out").printWriter().use { out ->
+        arr.forEach {
+            out.print(it)
+        }
+    }
+}
+
+private fun generateSymbol(arr: Array<Pair<Char, Double>?>, size: Int) {
+    val randomSize = (size..size * (1..10).random()).random()
+
+    val chrList = mutableListOf<Char>()
+    var count: Int
+    for (i in arr.indices) {
+        count = (randomSize * arr[i]!!.second).roundToInt()
+        (0 until count).forEach { _ ->
+            chrList.add(arr[i]!!.first)
+        }
+    }
+    chrList.shuffle()
+    writeListToFile(chrList)
+}
+
+private fun probabilityGenerate(assMap: MutableMap<Char, Int>) {
     var allChr = 0
-    assMap.forEach { (k, v) -> allChr += v }
+    assMap.forEach { (_, v) -> allChr += v }
 
-    val arrProb = arrayOfNulls<Double>(allChr)
-    for (i in 0 until allChr) //replay allChr to anymore ((0..10).rand())
-        arrProb[i] = Random.nextDouble()
-    arrProb.sort()
-
-//    val arrAssMap = arrayOfNulls<Double>(allChr)
-
-    val arrAssMap = mutableMapOf<Char, Double>()
-//    println("\tHISTOGRAM")
-//    assMap.toList().forEachIndexed { index, element ->
-//        val id = getEmptyIndex(arrAssMap)
-//        if (id != null)
-//            for (i in id until id + element.second)
-//                if (arrAssMap[i] == null)
-//                    arrAssMap[i] = element.second / allChr.toDouble()
-////        println("id === $id")
-//        println("Char: ${element.first} Prob: ${element.second / allChr.toDouble()}")
-//    }
-    println(arrAssMap.toList())
-
-
-
-
-
-
-//    val outFile = File("input" + ".out").bufferedWriter()
-//    println(allChr)
+    val arrAssMap: Array<Pair<Char, Double>?> = arrayOfNulls(assMap.size)
+    println("\tHISTOGRAM")
+    assMap.toList().forEachIndexed { _, element ->
+        val id = getEmptyIndexArr(arrAssMap)
+        if (id != null)
+            arrAssMap[id] = Pair(element.first, element.second / allChr.toDouble())
+        println("Char: ${element.first} Prob: ${element.second / allChr.toDouble()}")
+    }
+    generateSymbol(arrAssMap, allChr)
 }
 
 object Histogram {
     @JvmStatic
     fun main(arg: Array<String>) {
-        val map = File("src/main/kotlin/com/github/galads/sem02/input")
+        if (arg.size != 1) {
+            println("Error: not or more argument")
+            return
+        }
+        val map = File(arg[0])
             .readLines()
-            .map {  it  }
+            .map { it }
 
         val assMap = mutableMapOf<Char, Int>()
         for (str in map) {
             for (chr in str)
                 assMap[chr] = arrCountCharacters(map, chr)
         }
-        probabilityGenerate(map, assMap)
+        probabilityGenerate(assMap)
     }
 }
