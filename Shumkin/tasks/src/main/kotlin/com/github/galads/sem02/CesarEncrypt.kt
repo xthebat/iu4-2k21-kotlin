6bc823dd3db51c21bb3dbe6d9c443756a7da9664
Galads <nikola_2212@mail.ru>
2021-09-15 23:13:03 +0300

    fixed cesar method encrypt

--- Shumkin/tasks/src/main/kotlin/com/github/galads/sem02/CesarEncrypt.kt
+++ Shumkin/tasks/src/main/kotlin/com/github/galads/sem02/CesarEncrypt.kt
@@ -2,51 +2,52 @@ package com.github.galads.sem02
 
 import java.io.File
 
-//private fun encrypt(list: List<Char>, key: Int, buffer: MutableList<Char>) {
-////    val buffer = mutableListOf<Char>()
-//    list.forEach {
-//        when {
-//            it.isLetter() -> when {
-//                it + key in 'A'..'Z' -> buffer.add(it + key)
-//                it + key > 'Z' -> {
-//                    var value = it + key
-//                    val range = ('A'..'Z')
-//                    while (value !in range) {
-//                        value -= range.count()
-//                    }
-//                    buffer.add(value)
-//                }
-//            }
-//            else -> buffer.add(it)
-//        }
-//    }
-//}
+private fun mathEncrypt(chr: Char, start: Char, end: Char, buffer: MutableList<Char>, key: Int) {
+    when {
+        chr + key in start..end -> buffer.add(chr + key)
+        chr + key > end -> {
+            var value = chr + key
+            val range = (start..end)
+            while (value !in range) {
+                value -= range.count()
+            }
+            buffer.add(value)
+        }
+    }
+}
 
-private fun mathEncrypt(chr: Char,range: CharRange ) {
-
+private fun mathDecrypt(chr: Char, start: Char, end: Char, buffer: MutableList<Char>, key: Int) {
+    when {
+        chr - key in start..end -> buffer.add(chr - key)
+        else -> {
+            var value = chr - key
+            val range = (start..end)
+            while (value !in range) {
+                value += range.count()
+            }
+            buffer.add(value)
+        }
+    }
 }
 
 private fun encrypt(list: List<Char>, key: Int, buffer: MutableList<Char>) {
     list.forEach {
-        when {
-            it.isLetter() -> when {
-                it + key in 'A'..'Z' -> buffer.add(it + key)
-                it + key > 'Z' -> {
-                    var value = it + key
-                    val range = ('A'..'Z')
-                    while (value !in range) {
-                        value -= range.count()
-                    }
-                    buffer.add(value)
-                }
-            }
+        when (it) {
+            in ('A'..'Z') -> mathEncrypt(it, 'A', 'Z', buffer, key)
+            in ('a'..'z') -> mathEncrypt(it, 'a', 'z', buffer, key)
             else -> buffer.add(it)
         }
     }
 }
 
-private fun uncrypt(list: List<Char>, key: Int, buffer: MutableList<Char>) {
-
+private fun decrypt(list: List<Char>, key: Int, buffer: MutableList<Char>) {
+    list.forEach {
+        when (it) {
+            in ('A'..'Z') -> mathDecrypt(it, 'A', 'Z', buffer, key)
+            in ('a'..'z') -> mathDecrypt(it, 'a', 'z', buffer, key)
+            else -> buffer.add(it)
+        }
+    }
 }
 
 object CesarEncrypt {
@@ -56,31 +57,20 @@ object CesarEncrypt {
             println("Error: not a valid arguments : need 4 args")
             return
         }
-/////////////////////////
-        var str = ('С' + 0)
-//        println(str.isLetter())
-        println("""----> ${str.code}""")
-        if (str > 'Я') {
-            str = 'А'
-        }
-        println("----> $str")
-//////////////////////////
-        val listAlphaIn = mutableListOf<Char>()
 
-        File(arg[1])
-            .readText()
+        val listAlphaIn = mutableListOf<Char>()
+        File(arg[1]).readText()
             .let { it ->
                 it.forEach { listAlphaIn.add(it) }
             }
+
         val buffer = mutableListOf<Char>()
-
-
         when {
             arg[0].equals("encrypt", true) -> encrypt(listAlphaIn, arg[3].toInt(), buffer)
-            arg[0].equals("uncrypt", true) -> uncrypt(listAlphaIn, arg[3].toInt(), buffer)
+            arg[0].equals("decrypt", true) -> decrypt(listAlphaIn, arg[3].toInt(), buffer)
             else -> println("Error: not a valid key argument")
         }
-        println(buffer)
+
         File(arg[2]).printWriter().use { out ->
             buffer.forEach {
                 out.print(it)
@@ -88,3 +78,5 @@ object CesarEncrypt {
         }
     }
 }
+// Test case
+// encrypt src/main/kotlin/com/github/galads/sem02/Cesar.input src/main/kotlin/com/github/galads/sem02/output.file 751
