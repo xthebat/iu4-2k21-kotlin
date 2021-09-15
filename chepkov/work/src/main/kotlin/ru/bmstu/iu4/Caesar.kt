28d83c8b621effb0f2e6ce3a5bafe2b03b8aca4a
domhathair <domhathair@pm.me>
2021-09-15 15:45:35 +0300

    Caesar cipher done!

--- chepkov/work/src/main/kotlin/ru/bmstu/iu4/Caesar.kt
+++ chepkov/work/src/main/kotlin/ru/bmstu/iu4/Caesar.kt
@@ -8,16 +8,16 @@ object Caesar {
 
     private fun encode(input: String, shift: Int): String = input.map {
         when (it) {
-            in lower -> lower[(lower.indexOf(it) + shift) % 26]
-            in upper -> upper[(upper.indexOf(it) + shift) % 26]
+            in lower -> lower[(lower.indexOf(it) + shift) % lower.size]
+            in upper -> upper[(upper.indexOf(it) + shift) % upper.size]
             else -> it
         }
     }.joinToString("")
 
     private fun decode(input: String, shift: Int): String = input.map {
         when (it) {
-            in lower -> lower[(lower.indexOf(it) - shift + 26) % 26]
-            in upper -> upper[(upper.indexOf(it) - shift + 26) % 26]
+            in lower -> lower[(lower.indexOf(it) - shift + lower.size) % lower.size]
+            in upper -> upper[(upper.indexOf(it) - shift + upper.size) % upper.size]
             else -> it
         }
     }.joinToString("")
@@ -30,7 +30,7 @@ object Caesar {
             endMessage = "Encoding complete!"
         } else if (args[0] == "decode") {
             File(args[2]).writeText(decode(File(args[1]).readText(), args[3].toInt()))
-            endMessage = "Encoding complete!"
+            endMessage = "Decoding complete!"
         }
         print("$endMessage Check ${args[2]} to see result.")
     }
