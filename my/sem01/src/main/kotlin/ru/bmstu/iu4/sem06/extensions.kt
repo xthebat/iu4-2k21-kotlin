d71bf1727f12d4bfc870c0d5cd539862957b05eb
Alexei Gladkikh <a.gladkikh@inforion.ru>
2021-10-22 11:03:18 +0300

    Added sem08 code

--- my/sem01/src/main/kotlin/ru/bmstu/iu4/sem06/extensions.kt
+++ my/sem01/src/main/kotlin/ru/bmstu/iu4/sem06/extensions.kt
@@ -2,6 +2,42 @@
 
 package ru.bmstu.iu4.sem06
 
+import io.javalin.Javalin
+import io.javalin.http.Context
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.ZoneId
+import java.util.*
+
+
 inline fun String.splitTrim(delimiter: String) = split(delimiter).map { it.trim() }
 
 inline fun String.substringBetween(after: String, before: String) = substringAfter(after).substringBefore(before)
+
+inline fun Javalin.getSafe(path: String, crossinline action: Context.() -> Unit) {
+    get(path) { context ->
+        runCatching {
+            action(context)
+        }.getOrElse { error ->
+            context.status(500)
+            context.result(error.toString())
+        }
+    }
+}
+
+inline fun Javalin.postSafe(path: String, crossinline action: Context.() -> Unit) {
+    post(path) { context ->
+        runCatching {
+            action(context)
+        }.getOrElse { error ->
+            context.status(500)
+            context.result(error.toString())
+        }
+    }
+}
+
+fun LocalDate.toDate(): Date = Date.from(
+    atStartOfDay()
+        .atZone(ZoneId.systemDefault())
+        .toInstant()
+)
---
+++ my/sem01/src/main/kotlin/ru/bmstu/iu4/sem06/types.kt
@@ -0,0 +1,3 @@
+package ru.bmstu.iu4.sem06
+
+typealias Aliases = Map<String, List<String>>
